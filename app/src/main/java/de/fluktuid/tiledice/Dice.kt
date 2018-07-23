905300fd8e061f0732f6d5f630a4a5a9e395825f
Lukas f. Paluch <fluktuid@gmail.com>
2018-07-23 13:26:13 +0200

    Added twentysided dice

--- app/src/main/java/de/fluktuid/tiledice/Dice.kt
+++ app/src/main/java/de/fluktuid/tiledice/Dice.kt
@@ -113,16 +113,29 @@ class TwelveSideDice(@IntRange(from = 1, to = 12) private val eyes: Int) : Dice 
     override val contentDescription: Int = R.string.app_name
 }
 
-// TODO: Logos schreiben
 class TwentySideDice(@IntRange(from = 1, to = 20) private val eyes: Int) : Dice {
     @DrawableRes
     override val drawableRes: Int = when (eyes) {
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        6 -> R.drawable.dice_6
+        1 -> R.drawable.dice_d20_1
+        2 -> R.drawable.dice_d20_2
+        3 -> R.drawable.dice_d20_3
+        4 -> R.drawable.dice_d20_4
+        5 -> R.drawable.dice_d20_5
+        6 -> R.drawable.dice_d20_6
+        7 -> R.drawable.dice_d20_7
+        8 -> R.drawable.dice_d20_8
+        9 -> R.drawable.dice_d20_9
+        10 -> R.drawable.dice_d20_10
+        11 -> R.drawable.dice_d20_11
+        12 -> R.drawable.dice_d20_12
+        13 -> R.drawable.dice_d20_13
+        14 -> R.drawable.dice_d20_14
+        15 -> R.drawable.dice_d20_15
+        16 -> R.drawable.dice_d20_16
+        17 -> R.drawable.dice_d20_17
+        18 -> R.drawable.dice_d20_18
+        19 -> R.drawable.dice_d20_19
+        20 -> R.drawable.dice_d20_20
         else -> R.drawable.dice_multiple
     }
     @StringRes
